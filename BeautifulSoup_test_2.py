5d12c3c8cb7b1944a33c4b3d2c06e5e60cc5878b
Yeah-Kun <767786685@qq.com>
2017-03-13 23:15:15 +0800

    learn beautifulsoup

---
+++ BeautifulSoup_test_2.py
@@ -0,0 +1,5 @@
+from urllib.request import urlopen 
+from bs4 import BeautifulSoup
+html = urlopen("http://www.pythonscraping.com/pages/page1.html") 
+bsObj = BeautifulSoup(html.read()) 
+print(bsObj.h1)
