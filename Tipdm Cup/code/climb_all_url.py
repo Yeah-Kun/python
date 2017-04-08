573f8603c7c02d1a968defef858e39fdcd8e15f3
Yeah-Kun <767786685@qq.com>
2017-04-08 19:45:28 +0800

    update the program

--- Tipdm Cup/code/climb_all_url.py
+++ Tipdm Cup/code/climb_all_url.py
@@ -1,25 +1,45 @@
-from urllib.request import urlopen,HTTPError,URLError
+from urllib.request import urlopen, HTTPError, URLError
 import os
 import pickle
+from chardet.universaldetector import UniversalDetector
+import re
+from bs4 import BeautifulSoup as bs
+
+# 编码格式识别
+def Detection_coding_format(html):
+    detector = UniversalDetector()
+    for line in html.readlines():
+        detector.feed(line)
+        if detector.done:
+            break
+    detector.close()
+    return detector.result['encoding']
+
+
 # open the jar
 all_url_file = open(
     "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url.pkl", "rb")
 
 url = pickle.load(all_url_file)  # Take the pickles out of the jar
-
 n = 0
 for each_url in url:
     try:
         req = urlopen(each_url)
-        all_url_data = req.read()
-        pickle_file = open(
-            "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url_data.pkl", "ab")
-        pickle.dump(all_url_data,pickle_file)
-        pickle_file.close()
+        chardet = Detection_coding_format(req)
+        req = urlopen(each_url)
+        bs0bj = bs(req,from_encoding=chardet).get_text()
+        with open("D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url_data.pkl", "ab") as pickle_file:
+        	pickle.dump(bs0bj, pickle_file)
+        	pickle_file.close()
         n += 1
         print(n)
-    except (HTTPError,ConnectionResetError,URLError)  as reason:
+    except (HTTPError, ConnectionResetError, URLError) as reason:
+        error_file = open(
+            "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url_error_new.txt", "a")
+        n += 1
+        reason = str(n) + '.' + str(reason) + '\r'
+        error_file.writelines(reason)
+        error_file.close()
         print(reason)
-        pickle_file.close()
         continue
 all_url_file.close()
--- Tipdm Cup/code/save_All_message.py
+++ Tipdm Cup/code/save_All_message.py
@@ -2,13 +2,10 @@ from urllib.request import urlopen, HTTPError, URLError
 from bs4 import BeautifulSoup as bs   # Extract content
 import pickle    # save file
 from chardet.universaldetector import UniversalDetector
-
-all_url_file = open(
-    "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url.pkl", "rb")
-
-# Analysis of Web site coding format
+import re
 
 
+# 编码格式识别
 def Detection_coding_format(html):
     detector = UniversalDetector()
     for line in html.readlines():
@@ -18,36 +15,52 @@ def Detection_coding_format(html):
     detector.close()
     return detector.result['encoding']
 
-# find the title in the page code
 
-
+# 爬取标题
 def Find_title(bs0bj):
     title = bs0bj.find('title').get_text()
-    title = str(n) + '.' + str(title) + '\r'
+    title = str(title) + '\r'
     return title
 
+
+#  爬取时间
 def Find_date(bs0bj):
-    
+    date = re.search("发表于 \d{4}-\d{2}-\d{2}", bs0bj.get_text())
+    if date == None:
+        date = re.search("\d{4}/\d{2}/\d{2}", bs0bj.get_text())
+    if date == None:
+        date = re.search("\d{2}-\d{2}-\d{4}", bs0bj.get_text())
+    return date
+
+# 爬取正文
+def Find_maindata(bs0bj):
+
+
+path = "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\"
+all_url_file = open(
+    path + "all_url.pkl", "rb")
 
 html = pickle.load(all_url_file)
 n = 0
+
 for each in html:
     try:
         each_html = urlopen(each)  # open the page code
-        chardet = Detection_coding_format(each_html)# find the page encoding format
+        # find the page encoding format
+        chardet = Detection_coding_format(each_html)
         each_html = urlopen(each)
-        bs0bj = bs(each_html)    
+        bs0bj = bs(each_html)
         title = Find_title(bs0bj)
 
         save_file = open(
-            "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url_title_new.txt", "a")
+            path + "all_url_title_new.txt", "a")
         save_file.writelines(title)
         save_file.close()
         n += 1
         print(n)
     except (HTTPError, ConnectionResetError, URLError, AttributeError, TypeError, UnicodeEncodeError) as reason:
         error_file = open(
-            "D:\\Users\\YeahKun\\Desktop\\TDcup\\data process\\all_url_error_new.txt", "a")
+            path + "all_url_error_new.txt", "a")
         n += 1
         reason = str(n) + '.' + str(reason) + '\r'
         error_file.writelines(reason)
